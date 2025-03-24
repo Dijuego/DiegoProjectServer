a0b929c7892a1d8d6d02dbb180fc6baed4663db1
Dijuego <diego.hernandez.735@my.csun.edu>
2025-03-24 14:14:40 -0700

    Initialized Server, Created Server, Scaffolded using the Databse, Created the Player and Team Controllers

---
+++ Model/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model;
+
+[Table("Player")]
+public partial class Player
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    public int Number { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    [Column(TypeName = "decimal(2, 1)")]
+    public decimal Points { get; set; }
+
+    [Column(TypeName = "decimal(2, 1)")]
+    public decimal Rebunds { get; set; }
+
+    [Column(TypeName = "decimal(2, 1)")]
+    public decimal Assists { get; set; }
+
+    [StringLength(7)]
+    [Unicode(false)]
+    public string Minutes { get; set; } = null!;
+
+    [Column("TeamID")]
+    public int TeamId { get; set; }
+
+    [ForeignKey("TeamId")]
+    [InverseProperty("Players")]
+    public virtual Team Team { get; set; } = null!;
+}
---
+++ Model/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model;
+
+[Table("Team")]
+public partial class Team
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [MaxLength(50)]
+    public byte[] Name { get; set; } = null!;
+
+    [MaxLength(5)]
+    public byte[] Conference { get; set; } = null!;
+
+    public int Win { get; set; }
+
+    public int Loss { get; set; }
+
+    [InverseProperty("Team")]
+    public virtual ICollection<Player> Players { get; set; } = new List<Player>();
+}
